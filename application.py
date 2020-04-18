23580e02df436f6a2a2909b4af2ac595d1fce2b9
Chrismass <dollerbankas@gmail.com>
2020-04-17 17:58:39 -1200

    config file added

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 import os
+import config
 
 from flask import Flask, session
 from flask_session import Session
@@ -8,7 +9,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 app = Flask(__name__)
 
 # Check for environment variable
-if not os.getenv("DATABASE_URL"):
+if not os.getenv("config.DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
